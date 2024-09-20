363a3fbbb6c9465c1f98b2ffac023469ad153d59
maninderpal888 <165861990+maninderpal888@users.noreply.github.com>
2024-09-20 13:07:41 +1200

    Add files via upload

---
+++ week 2/04.py
@@ -0,0 +1,6 @@
+"""
+program which calculates the area of a circle Author: Maninderpal singh"""
+radius = float(input("Enter the radius:"))
+pi = 3.14159265359
+area = pi* radius **2
+print("Area of a circle is", area)
---
+++ week 2/lab2.py
@@ -0,0 +1,10 @@
+"""author : maninderpal """
+name = "money"
+extra = 3
+
+name_length =len(name)
+stars_length = name_length + extra *2
+
+print("*"*stars_length)
+print(""*extra + name + ""*extra)
+print("*"*stars_length)
