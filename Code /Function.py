2d04b5cedd6c897ca6d433dfc32802c3baccc403
maninderpal888 <165861990+maninderpal888@users.noreply.github.com>
2024-09-25 12:08:50 +0530

    Delete Code  directory

--- Code /Function.py
+++
@@ -1,9 +0,0 @@
-def create_group(person1, person2):
-    group = [person1, person2]
-    return group
-
-person1 = "Maninderpal"
-person2 = "Ankit"
-
-my_group = create_group(person1, person2)
-print(f"Group members: {my_group}")
